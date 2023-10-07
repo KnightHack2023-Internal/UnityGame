1c277a94be4cd7c32e0f5c6c6abac8227135480f
zhiyan114 <zhiyan114@zhiyan114.com>
2023-10-07 13:16:08 -0400

    Added objects and movement script

---
+++ Assets/BossMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Change position one to the left
+        transform.position = transform.position + new Vector3((float)-0.0001, 0, 0);
+    }
+}
